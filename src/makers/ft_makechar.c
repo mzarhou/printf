e6d9bb2e2b61570d04a2d5a5fa71ab5aa6ddde54
mzarhou <mzarhou.cc@gmail.com>
2021-11-25 03:47:08 +0100

    fix: c

--- src/makers/ft_makechar.c
+++ src/makers/ft_makechar.c
@@ -6,7 +6,7 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 21:47:05 by mzarhou           #+#    #+#             */
-/*   Updated: 2021/11/25 02:12:59 by mzarhou          ###   ########.fr       */
+/*   Updated: 2021/11/25 03:35:59 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,21 @@ t_list	*ft_makechar(t_list **list_ptr, va_list args, const t_format *format)
 {
 	char	c;
 	char	*str;
+	int		len;
 
 	c = va_arg(args, int);
 	str = ft_char2str(c);
 	str = ft_assign_free(str, make_it(str, format));
 	if (c == 0)
 	{
+		len = ft_strlen(str);
+		if (len > 0)
+			str[len - 1] = 0;
+		if (format->flags.minus)
+		{
+			ft_lstadd_back(list_ptr, ft_lstnew(0));
+			return (ft_lstnew(str));
+		}
 		ft_lstadd_back(list_ptr, ft_lstnew(str));
 		return (ft_lstnew(0));
 	}
